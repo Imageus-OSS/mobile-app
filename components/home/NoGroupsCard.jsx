94f4095f8e1f1bae781298f9b41184c3189f3179
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-12 18:13:24 -0400

    Initial commit

---
+++ components/home/NoGroupsCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Prototypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
+
+function NoGroupsCard({}) {
+    return (
+        <View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default NoGroupsCard;
