2bab266ad610fa20befff24909c13d1de0cb2305
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-15 13:34:44 -0400

    Added Navbar to card

--- components/home/NoGroupsCard.jsx
+++ components/home/NoGroupsCard.jsx
@@ -2,29 +2,42 @@ import React from 'react';
 import Prototypes from 'prop-types';
 import { View, Text, StyleSheet, Alert } from 'react-native';
 import Button from '../Button';
+import Navbar from '../Navbar';
 
 function NoGroupsCard({ }) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title} >You're Missing Out!</Text>
-            {/** placeholder for icon in Figma */}
-            <View>
-                <View style={styles.icon} ></View>
+        <View style={styles.background}>
+            <View style={styles.header}>
+                <Navbar title='ImageUs' initials='JT' />
             </View>
-            <View>
-                <Text style={styles.paragraph}>Join some groups to start sharing photos</Text>
-                <Button title="Join Group" onPress={() => Alert.alert("Join Group!")}/>
-                <Button title="Create Group" onPress={() => Alert.alert("Create Group!")}/>
+            <View style={styles.container}>
+                <Text style={styles.title} >You're Missing Out!</Text>
+                {/** placeholder for icon in Figma */}
+                <View>
+                    <View style={styles.icon} ></View>
+                </View>
+                <View>
+                    <Text style={styles.paragraph}>Join some groups to start sharing photos</Text>
+                    <Button title="Join Group" onPress={() => Alert.alert("Join Group!")} />
+                    <Button title="Create Group" onPress={() => Alert.alert("Create Group!")} />
+                </View>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    background: {
+        position: 'absolute',
+        top: 0,
+    },
     container: {
         alignContent: 'center',
         justifyContent: 'center',
-        //marginTop: '40%', assumed the landing card for this will place it correctly
+        marginTop: '40%', //assumed the landing card for this will place it correctly
+    },
+    header: {
+        top: 10,
     },
     title: {
         fontFamily: 'Poppins_600SemiBold',
