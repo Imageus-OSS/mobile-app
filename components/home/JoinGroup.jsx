88a65b25e971f365727fa5a2152da11c8129de5d
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-15 14:11:47 -0400

    Figma Join Group Design, button style not working as usual, no shadow since Android does not support it

--- components/home/JoinGroup.jsx
+++ components/home/JoinGroup.jsx
@@ -3,15 +3,31 @@ import Prototypes from 'prop-types';
 import { View, Text, StyleSheet, Alert } from 'react-native';
 import Button from '../Button';
 import Navbar from '../Navbar';
+import Input from '../Input';
+//import InputStyles from '../../styles/InputStyles';
 
 function JoinGroup({ }) {
     return (
         <View style={styles.background}>
-        <View style={styles.header}>
-            <Navbar title='ImageUs' initials='JT' />
-            <View>
+            <View style={styles.header}>
+                <Navbar title='ImageUs' initials='JT' />
+                <View style={styles.container}>
+                    <View style={styles.titleView}>
+                        <Text style={styles.title}>Join Group</Text>
+                    </View>
+                    <Text style={styles.subtitle}>Group Code</Text>
+                    <Input placeholder='example-code'></Input>
+                    <View style={styles.icon}></View>
+                </View>
+                <View style={styles.buttonCard}>
+                    <View style={styles.button}>
+                        <Button title='Join Group' />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Cancel' />
+                    </View>
+                </View>
             </View>
-        </View>
         </View>
     )
 }
@@ -23,6 +39,47 @@ const styles = StyleSheet.create({
     },
     header: {
         top: 10,
+    },
+    container: {
+        margin: 15,
+    },
+    titleView: {
+        marginTop: 15,
+        marginLeft: 5,
+        marginBottom: 30,
+    },
+    title: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        width: '100%',
+    },
+    subtitle: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        width: '100%',
+        marginLeft: 5,
+        marginBottom: 10,
+    },
+    icon: {
+        height: 150,
+        width: 150,
+        backgroundColor: 'grey',
+        margin: 80,
+        alignSelf: 'center',
+    },
+    buttonCard: {
+        marginTop: 10,
+        padding: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        elevation: 2.5, // shadow does not work on iOS so I can't make it
+    },
+    button: {
+        margin: 10,
     },
 })
 
