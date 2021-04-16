7a1290c66603e1d3c7e36467745699986ec01def
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-16 11:59:26 -0400

    Completed card for reuse among home pages

--- components/BottomButtonCard.jsx
+++ components/BottomButtonCard.jsx
@@ -3,14 +3,30 @@ import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 import Button from './Button';
 
-function BottomButtonCard({}) {
-    return(
-        <View>
-
+function BottomButtonCard({ TopButtonText, BottomButtonText, onPressTop, onPressBottom }) {
+    return (
+        <View style={styles.buttonCard}>
+            <View style={styles.button}>
+                <Button title={TopButtonText} onPress={onPressTop} />
+            </View>
+            <View style={styles.button}>
+                <Button title={BottomButtonText} onPress={onPressBottom} />
+            </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    buttonCard: {
+        marginTop: 10,
+        padding: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        elevation: 2.5, // shadow does not work on iOS so I can't make it
+    },
+    button: {
+        margin: 10,
+    },
+});
 
 export default BottomButtonCard;
