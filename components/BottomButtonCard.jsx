de52fdfc43d65d50a17465f415c3b04660575adf
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-16 11:55:59 -0400

    Initial Commit

---
+++ components/BottomButtonCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
+import Button from './Button';
+
+function BottomButtonCard({}) {
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default BottomButtonCard;
