3381dfae0b913e50f7132a38df8e9de580420b67
AustinTillotson <austintillotson@knights.ucf.edu>
2021-04-22 10:55:31 -0400

    Initial Commit

---
+++ src/components/main/CameraScreen.jsx
@@ -0,0 +1,13 @@
+import React, { useState, userEffect } from 'react';
+import { View, Text } from 'react-native';
+import { Camera } from 'expo-camera';
+
+function CameraScreen() {
+    return (
+        <View>
+            <Text>test</Text>
+        </View>
+    );
+}
+
+export default CameraScreen;
